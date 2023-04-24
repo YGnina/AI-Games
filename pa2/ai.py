29c398b5e2c54691c2a99ddd02eeec312540a176
YGnina <93010504+YGnina@users.noreply.github.com>
2023-04-24 15:54:07 -0700

    Update ai.py

--- pa2/ai.py
+++ pa2/ai.py
@@ -6,6 +6,7 @@ from math import inf
 
 MOVES = {0: 'up', 1: 'left', 2: 'down', 3: 'right'}
 MAX_PLAYER, CHANCE_PLAYER = 0, 1 
+# reach 512 tiles and a score over 5000
 
 # Tree node. To be used to construct a game tree. 
 class Node: 
@@ -23,6 +24,7 @@ class Node:
         #TODO: complete this
         #pass
         return len(self.children) == 0
+    
 
 # AI agent. Determine the next move.
 class AI:
@@ -41,38 +43,46 @@ class AI:
         if node == None:
             node = self.root
 
-        if depth >= 0:
-            if node.player_type == CHANCE_PLAYER: #Game simulator
+        if depth < 0:
+            return
+        
+        if node.player_type == CHANCE_PLAYER: #Game simulator
 
-                #Place a random tile a 2 in an empty tile - get_open_tiles() returns all empty tiles
-                empty_tile = self.simulator.get_open_tiles()
-                self.simulator.set_state(self.simulator.get_state()[0],self.simulator.get_state()[1])
-                #print(self.simulator.get_state()[1])
-                # #Child nodes = #empty tiles
-                for x,y in empty_tile:
-                    self.simulator.tile_matrix[x][y] = 2
-                    child =  Node(self.simulator.current_state(),MAX_PLAYER) #child shoud have different player type
-                    node.children.append(child)
-                    self.build_tree(child,depth-1)
+            #Place a random tile a 2 in an empty tile - get_open_tiles() returns all empty tiles
+            empty_tile = self.simulator.get_open_tiles()
+            #self.simulator.set_state(self.simulator.get_state()[0],self.simulator.get_state()[1])
+            # self.simulator.set_state(node.state[0],node.state[1])
+            #print(self.simulator.get_state()[1])
+            # #Child nodes = #empty tiles
+            for x,y in empty_tile:
+                self.simulator.set_state(node.state[0],node.state[1])
+                self.simulator.tile_matrix[x][y] = 2
+                child = Node(self.simulator.current_state(),MAX_PLAYER) #child shoud have different player type
+                node.children.append(child)
+                # print(child)
+                self.build_tree(child,depth-1)
+                self.simulator.set_state(node.state[0], node.state[1])
 
-                # Likelihood of transition = 1/(#empty tiles)
-                likelihood = 1/len(empty_tile)      #?
-            
+            # Likelihood of transition = 1/(#empty tiles) -> chance()
 
-            elif node.player_type == MAX_PLAYER:
-                # 4 actions: 0 - up, 1 - left, 2 - down, 3 - right
-                for m in MOVES:
-                    self.simulator.set_state(self.simulator.get_state()[0],self.simulator.get_state()[1])
-                    if(self.simulator.move(m)):
-                        child = Node(self.simulator.current_state(), CHANCE_PLAYER)
-                        node.children.append(child)
-                        self.build_tree(child, depth-1)
-                    else:
-                        node.children.append(Node(self.simulator.current_state(), 2))
-                    
-
+        elif node.player_type == MAX_PLAYER:
+            # 4 actions: 0 - up, 1 - left, 2 - down, 3 - right
+            for m in MOVES:
+                #self.simulator.set_state(self.simulator.get_state()[0],self.simulator.get_state()[1])
+                self.simulator.set_state(node.state[0],node.state[1])
                 # - Max node can have at most 4 children
                 # - Check if action is possible by using move() in game class - Returns True if valid
+                if(self.simulator.move(m)):
+                    child = Node(self.simulator.current_state(), CHANCE_PLAYER)
+                    node.children.append(child)
+                    self.build_tree(child, depth-1)
+
+                #     self.simulator.set_state(node.state[0], node.state[1])
+                # else:
+                #     node.children.append(Node(self.simulator.current_state(), 2))
+                    
+
+                
 
     def chance(self,node):
         return 1/len(node.children)     
@@ -85,26 +95,29 @@ class AI:
         
         if node.is_terminal():
             # If the node is a terminal node, return its score
-            return None, node.state[1]
-        
+            return (None, node.state[1])
+        if node is None:
+            node = self.root
+
         if node.player_type == MAX_PLAYER:
             value = -inf
-            direction = 0
-            for c in node.children:
+            direction = None
+            for d,c in enumerate(node.children):    #node.children:
                 new_value = max(value, self.expectimax(c)[1])   # max value
-                if(new_value >= value):
-                    value = new_value
-                    direction = random.randint(0, 3)    #c[0]
-            return direction,value
+                # if(new_value != value):
+                value = new_value
+                direction = d   #random.randint(0, 3)    #c[0]
+            return (direction,value)
 
         elif node.player_type == CHANCE_PLAYER:
             value = 0
             for c in node.children:
                 #value = max(value, self.expectimax(c)[1])
                 value += self.expectimax(c)[1] * self.chance(node)
-            return None,value
+                
+            return (None,value)
         else:
-            print("error")
+            return (None,0)
        
        # return random.randint(0, 3), 0
 
@@ -117,5 +130,8 @@ class AI:
 
     # TODO (optional): implement method for extra credits
     def compute_decision_ec(self):
-        return random.randint(0, 3)
+        # return random.randint(0, 3)
+        self.build_tree(self.root, self.search_depth)
+        direction, _ = self.expectimax(self.root)
+        return direction
 
